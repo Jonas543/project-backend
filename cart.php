35364d7618a9c88aa544a7b97e33672d7151f5b7
Jonas543 <vandenbosschejonas4@gmail.com>
2026-01-06 02:12:09 +0100

    frontend voor winkelmandje

---
+++ cart.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Winkelmandje - BikeStore</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<?php include_once("includes/header.inc.php"); ?>
+
+<main class="cart">
+    <h1>Winkelmandje</h1>
+    <div id="cart-items"></div>
+    <p id="cart-total"></p>
+    <button class="btn">Afrekenen</button>
+</main>
+
+<?php include_once("includes/footer.inc.php"); ?>
+</body>
+</html>
