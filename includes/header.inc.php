96c5773d308eab63e068f2660c1ae6538966341e
Jonas543 <vandenbosschejonas4@gmail.com>
2026-01-06 01:09:37 +0100

    header en footer gemaakt

---
+++ includes/header.inc.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="logo">BikeStore</div>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="products.php">Fietsen</a>
+        <a href="cart.php">Winkelmandje</a>
+        <a href="login.php">Login</a>
+    </nav>
+</header>
